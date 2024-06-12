254dd4c40fa10f5547c60215ccb9e2355f231186
luis-miro <96116501+luis-miro@users.noreply.github.com>
2024-06-12 13:33:15 +0200

    Update get_labels.py

--- python/get_labels.py
+++ python/get_labels.py
@@ -24,29 +24,31 @@ API_TOKEN = ''
 #     return csv_data
 
 def json_to_csv(json_data):
-    if json_data:
-        csv = ''
-        # Get the headers
-        headers = list(json_data[list(json_data.keys())[0]].keys())
-        csv += ','.join(headers) + '\n'
+    # Convert JSON keys to a list if they are not already in that format
+    if isinstance(json_data, dict):
+        json_data = [json_data[key] for key in json_data]
         
-        # Helper function to escape CSV special characters
-        def escape_csv(value):
-            if isinstance(value, (int, float)):
-                value = str(value)
-            if isinstance(value, str):
-                if '"' in value:
-                    value = value.replace('"', '""')
-                # if ',' in value or '"' in value or '\n' in value:
-            value = f'"{value}"'
-            return value
+    # Get the headers from the first element of the JSON data
+    headers = list(json_data[0].keys())
+    csv_data = ','.join(headers) + '\n'
+
+    # Helper function to escape CSV special characters
+    def escape_csv(value):
+        if isinstance(value, (int, float)):
+            value = str(value)
+        if isinstance(value, str):
+            if '"' in value:
+                value = value.replace('"', '""')
+            # if ',' in value or '"' in value or '\n' in value:
+        value = f'"{value}"'
+        return value
+
+    # Add the data
+    for row in json_data:
+        data = ','.join(escape_csv(json_data[row][header]) for header in headers)
+        csv_data += data + '\n'
         
-        # Add the data
-        for row in json_data:
-            data = ','.join(escape_csv(json_data[row][header]) for header in headers)
-            csv += data + '\n'
-        
-        return csv
+    return csv
 
 async def call_api(url, options):
     async def manage_errors(response):
